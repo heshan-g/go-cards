fa421284d7ff066615659360f5631c4031f0b9dc
Heshan <heshan.ua@gmail.com>
2022-12-13 01:54:54 +0530

    Add test TestSaveToDeckAndNewDeckTestFromFile

--- deck_test.go
+++ deck_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
@@ -16,4 +19,19 @@ func TestNewDeck(t *testing.T) {
 	if d[len(d) - 1] != "Four of Clubs" {
 		t.Errorf("Expected last card to be Four of  lubs, but got %v", d[len(d) - 1])
 	}
+}
+
+func TestSaveToDeckAndNewDeckTestFromFile(t *testing.T) {
+	os.Remove("_decktesting")
+
+	desk := newDeck()
+	desk.saveToFile(("_decktesting"))
+
+	loadedDeck := newDeckFromFile("_decktesting")
+
+	if len(loadedDeck) != 16 {
+		t.Errorf("Expected 16 cards in deck, got %v", len(loadedDeck))
+	}
+
+	os.Remove("_decktesting")
 }
