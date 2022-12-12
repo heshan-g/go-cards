92a041a5318aab457dd1dbd81ebe02f09cdf89cf
Heshan <heshan.ua@gmail.com>
2022-12-12 18:41:36 +0530

    Add newDeckFromFile

--- main.go
+++ main.go
@@ -1,17 +1,18 @@
 package main
-
-import "fmt"
  
 func main() {
-	cards := newDeck()
+	// cards := newDeck()
 
+	// cards.print()
+	// fmt.Println("------------------------")
+
+	// hand, remainingDeck := deal(cards, 5)
+	// hand.print()
+	// remainingDeck.print()
+	// fmt.Println("------------------------")
+
+	// cards.saveToFile("my_cards")
+
+	cards := newDeckFromFile("my_cards")
 	cards.print()
-	fmt.Println("------------------------")
-
-	hand, remainingDeck := deal(cards, 5)
-	hand.print()
-	remainingDeck.print()
-	fmt.Println("------------------------")
-
-	cards.saveToFile("my_cards")
 }
